ba59c55686ea09487630cabe9e3e89db33b04aff
Seok_Soo <whdtn7855@naver.com>
2026-07-24 09:07:06 +0900

    fix: app.jsx 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import BoardListPage from './features/board/pages/BoardListPage';
 import BoardDetailPage from './features/board/pages/BoardDetailPage';
 import BoardWritePage from './features/board/pages/BoardWritePage';
@@ -21,34 +27,31 @@ import WalkCalendarPage from './features/walk/pages/WalkCalendarPage';
 import WalkTrackingPage from './features/walk/pages/WalkTrackingPage';
 import WalkDetailPage from './features/walk/pages/WalkDetailPage';
 import WalkRouteMapPage from './features/walk/pages/WalkRouteMapPage';
-import { useEffect } from 'react';
 
 const KAKAO_KEY = import.meta.env.VITE_KAKAO_MAP_KEY;
 
-function App() { //테스트용
-
+function App() {
   useEffect(() => {
     // 이미 로드되어 있으면 다시 로드하지 않음
     if (window.kakao && window.kakao.maps) return;
 
-    const script = document.createElement("script");
+    const script = document.createElement('script');
     script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_KEY}&autoload=false`;
     script.async = true;
 
     document.head.appendChild(script);
   }, []);
 
-
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/map" element={<MapPage />} />
+    <BrowserRouter>
+      <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         {/* <Route path="/verify" element={<VerificationPage />} /> */}
 
         <Route element={<RequireAuth />}>
+          <Route path="/map" element={<MapPage />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/mypage/edit" element={<ProfileEditPage />} />
           <Route path="/mypage/favorites" element={<FavoritesPage />} />
@@ -62,17 +65,17 @@ function App() { //테스트용
           <Route path="/mypage/groups/:groupId/edit" element={<GroupFormPage />} />
           <Route path="/board" element={<BoardListPage />} />
           <Route path="/board/write" element={<BoardWritePage />} />
+          <Route path="/board/:id" element={<BoardDetailPage />} />
           <Route path="/walk" element={<WalkCalendarPage />} />
-          <Route path="/walk/tracking/:walkId" element={<WalkTrackingPage />}/>
+          <Route path="/walk/tracking/:walkId" element={<WalkTrackingPage />} />
           <Route path="/walk/:walkId" element={<WalkDetailPage />} />
-          <Route path="/walk/:walkId/map" element={<WalkRouteMapPage />}/>
-          {/* <Route path="/place" element={<PlacePage />} /> */}
+          <Route path="/walk/:walkId/map" element={<WalkRouteMapPage />} />
         </Route>
 
-          <Route path="*" element={<Navigate to="/login" replace />} />
-      </>
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
     </BrowserRouter>
   );
-          }
+}
 
 export default App;
